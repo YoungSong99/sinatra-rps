d6ecd3bca0713aa90baa644b6aa30a2c6739afc1
Young Song <58712406+YoungSong99@users.noreply.github.com>
2024-04-04 21:55:26 +0000

    feat: update the view, add new logic for the game

--- app.rb
+++ app.rb
@@ -1,31 +1,47 @@
 require "sinatra"
-require "sinatra/reloader"
-require "better_errors"
-require "binding_of_caller"
+require "sinatra/reloader" if development?
+require "better_errors" if development?
+require "binding_of_caller" if development?
 
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+end
 
-
-# get("/") do
-#   erb(:layout)
-# end
+get("/") do
+  erb(:main)
+end
 
 
 get("/rock") do
 
   @ours = "rock"
   @them = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb(:game)
+end
 
-  if @them == "rock"
+get("/paper") do
+  @ours = "paper"
+  @them = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb(:game)
+end
+
+get("/scissors") do
+  @ours = "scissors"
+  @them = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb(:game)
+end
+
+def determine_result
+  if @them == @ours
     @result = "tied"
-  elsif @them == "paper"
+  elsif (@them == "rock" && @ours == "scissors") || (@them == "scissors" && @ours == "paper") || (@them == "paper" && @ours == "rock")
     @result = "lost"
   else
     @result = "won"
   end
-
-  erb(:game)
-
 end

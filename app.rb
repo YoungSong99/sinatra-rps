b6653cc3e009fb6b7d1679f20a1f564dbfc962e6
Young Song <58712406+YoungSong99@users.noreply.github.com>
2024-04-04 21:30:16 +0000

    feat: add html

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+# get("/") do
+#   erb(:layout)
+# end
+
+
+get("/rock") do
+
+  @ours = "rock"
+  @them = ["rock", "paper", "scissors"].sample
+
+  if @them == "rock"
+    @result = "tied"
+  elsif @them == "paper"
+    @result = "lost"
+  else
+    @result = "won"
+  end
+
+  erb(:game)
+
 end
